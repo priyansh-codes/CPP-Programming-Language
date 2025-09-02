62eebc28fbd54f9ee40f2735d2d2a3ea4e58edd9
priyansh-codes <priyansh.is.coding@gmail.com>
2025-09-03 00:18:07 +0530

    Day-02: add operators and expressionss programs

---
+++ Day-02-Operators and Expressions/areaOfCircle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius, area ;
+
+    cout<<"Enter radius:- ";
+    cin>>radius;
+
+    area = 3.1425f * radius * radius;
+
+    cout<<"Area of the circle is:- "<<area;
+
+}
---
+++ Day-02-Operators and Expressions/compoundAssignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b= 5, c=5;
+    int sum = 5, compoundSum = 5;
+
+    sum = sum + a;
+    compoundSum += a;
+
+    cout<<"Regular sum:- "<<sum<<endl;
+    cout<<"Compund sum:- "<<compoundSum;
+
+
+}
